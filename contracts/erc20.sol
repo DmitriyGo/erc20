d6d199e81c75d791dbffb7fae28a386ecf00422d
Dmytro Horbatenko <dmytro.horbatenko@redduck.io>
2023-11-09 21:13:22 +0200

    Initial commmit

---
+++ contracts/erc20.sol
@@ -0,0 +1,65 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+contract MyToken is IERC20 {
+    uint256 private _totalSupply;
+    address public owner;
+
+    mapping(address => uint256) private _balances;
+    mapping(address => mapping(address => uint256)) private _allowances;
+
+    constructor(uint256 initialSupply) {
+        owner = msg.sender;
+        _totalSupply = initialSupply * 10 ** 18;
+        _balances[msg.sender] = _totalSupply;
+        emit Transfer(address(0), msg.sender, _totalSupply);
+    }
+
+    function totalSupply() external view override returns (uint256) {
+        return _totalSupply;
+    }
+
+    function balanceOf(address account) external view override returns (uint256) {
+        return _balances[account];
+    }
+
+    function transfer(address to, uint256 value) external override returns (bool) {
+        _transfer(msg.sender, to, value);
+        return true;
+    }
+
+    function allowance(address owner, address spender) external view override returns (uint256) {
+        return _allowances[owner][spender];
+    }
+
+    function approve(address spender, uint256 value) external override returns (bool) {
+        _approve(msg.sender, spender, value);
+        return true;
+    }
+
+    function transferFrom(address from, address to, uint256 value) external override returns (bool) {
+        _transfer(from, to, value);
+        _approve(from, msg.sender, _allowances[from][msg.sender] - value);
+        return true;
+    }
+
+    function _transfer(address sender, address recipient, uint256 amount) internal {
+        require(sender != address(0), "Transfer from the zero address");
+        require(recipient != address(0), "Transfer to the zero address");
+        require(_balances[sender] >= amount, "Transfer amount exceeds balance");
+
+        _balances[sender] -= amount;
+        _balances[recipient] += amount;
+        emit Transfer(sender, recipient, amount);
+    }
+
+    function _approve(address owner, address spender, uint256 amount) internal {
+        require(owner != address(0), "Approve from the zero address");
+        require(spender != address(0), "Approve to the zero address");
+
+        _allowances[owner][spender] = amount;
+        emit Approval(owner, spender, amount);
+    }
+}
---
+++ test/erc20.ts
@@ -0,0 +1,114 @@
+import { expect } from "chai";
+import { ethers } from "hardhat";
+import { Signer, parseEther, ZeroAddress } from "ethers";
+import { MyToken__factory, MyToken } from "../typechain-types";
+
+type SignerWithAddress = {
+  address: string;
+} & Signer;
+
+describe("MyToken", function () {
+  let myToken: MyToken;
+  let owner: SignerWithAddress;
+  let addr1: SignerWithAddress;
+  let addr2: SignerWithAddress;
+  let addrs: SignerWithAddress[];
+
+  beforeEach(async function () {
+    [owner, addr1, addr2, ...addrs] = await ethers.getSigners();
+
+    const myTokenFactory = (await ethers.getContractFactory("MyToken", owner)) as MyToken__factory;
+    const totalSupply = (10 ** 9).toString();
+    myToken = await myTokenFactory.deploy(parseEther(totalSupply));
+  });
+
+  describe("Deployment", function () {
+    it("Should set the right owner", async function () {
+      console.log(owner.address);
+      expect(await myToken.owner()).to.equal(owner.address);
+    });
+
+    it("Should assign the total supply of tokens to the owner", async function () {
+      const ownerBalance = await myToken.balanceOf(owner.address);
+      expect(await myToken.totalSupply()).to.equal(ownerBalance);
+    });
+  });
+
+  describe("Transactions", function () {
+    it("Should transfer tokens between accounts", async function () {
+      await myToken.transfer(addr1.address, 50);
+      const addr1Balance = await myToken.balanceOf(addr1.address);
+      expect(addr1Balance).to.equal(50);
+    });
+
+    it("Should fail if sender doesn’t have enough tokens", async function () {
+      const initialOwnerBalance = await myToken.balanceOf(owner.address);
+
+      await expect(myToken.connect(addr1).transfer(owner.address, 1)).to.be.revertedWith(
+        "Transfer amount exceeds balance"
+      );
+
+      expect(await myToken.balanceOf(owner.address)).to.equal(initialOwnerBalance);
+    });
+
+    it("Should update balances after transfers", async function () {
+      const initialOwnerBalance = await myToken.balanceOf(owner.address);
+
+      await myToken.transfer(addr1.address, 100);
+
+      await myToken.transfer(addr2.address, 50);
+
+      const finalOwnerBalance = await myToken.balanceOf(owner.address);
+      expect(finalOwnerBalance).to.equal(initialOwnerBalance - 150n);
+
+      const addr1Balance = await myToken.balanceOf(addr1.address);
+      expect(addr1Balance).to.equal(100);
+
+      const addr2Balance = await myToken.balanceOf(addr2.address);
+      expect(addr2Balance).to.equal(50);
+    });
+  });
+
+  describe("approve", function () {
+    it("should allow users to approve a spender", async function () {
+      const approveAmount = parseEther("100");
+
+      await expect(myToken.connect(addr1).approve(addr2.address, approveAmount))
+        .to.emit(myToken, "Approval")
+        .withArgs(addr1.address, addr2.address, approveAmount);
+
+      const allowance = await myToken.allowance(addr1.address, addr2.address);
+      expect(allowance).to.equal(approveAmount);
+    });
+
+    it("should fail when the spender is the zero address", async function () {
+      const approveAmount = parseEther("100");
+      await expect(myToken.connect(addr1).approve(ZeroAddress, approveAmount)).to.be.revertedWith(
+        "Approve to the zero address"
+      );
+    });
+  });
+
+  describe("transferFrom", function () {
+    it("should allow a spender to transfer tokens on behalf of the owner", async function () {
+      const transferAmount = parseEther("100");
+      await myToken.connect(owner).approve(addr1.address, transferAmount);
+      await expect(myToken.connect(addr1).transferFrom(owner.address, addr2.address, transferAmount))
+        .to.emit(myToken, "Transfer")
+        .withArgs(owner.address, addr2.address, transferAmount);
+
+      const finalBalance = await myToken.balanceOf(addr2.address);
+      expect(finalBalance).to.equal(transferAmount);
+    });
+
+    it("should fail when the spender does not have enough allowance", async function () {});
+
+    it("should fail when transferring from the zero address", async function () {
+      const transferAmount = parseEther("100");
+      await myToken.connect(owner).approve(addr1.address, transferAmount);
+      await expect(myToken.connect(addr1).transferFrom(ZeroAddress, addr2.address, transferAmount)).to.be.revertedWith(
+        "Transfer from the zero address"
+      );
+    });
+  });
+});
